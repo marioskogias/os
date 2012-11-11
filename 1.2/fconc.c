3d92e4706382fb15a2852a2af8fc78454bbbc4d4
marioskogias <marioskogias@gmail.com>
2012-11-11 14:26:53 +0200

    Started the new structure

--- 1.2/fconc.c
+++ 1.2/fconc.c
@@ -4,8 +4,23 @@
 #include <stdlib.h>
 #include<string.h>
 
-#define BUF_SIZE 1
+#define BUF_SIZE 256
 
+void doWrite(int fd, const char *buff, int len) {
+	write(fd, buff, len);
+}
+
+void write_file(int fd, const char *infile) {
+	int inf = open(infile, O_RDONLY); //file descriptor 1
+	char buf[BUF_SIZE];
+	if (inf<0) {
+		perror(infile);
+		exit(1);	
+	}
+	while ((r1 = read(fd1,buf,BUF_SIZE))>0)
+		doWrite(fd,buff,r1)
+
+}
 int main(int argc ,char **argv) {
 	
 	int fd1,fd2,out;
@@ -16,23 +31,11 @@ int main(int argc ,char **argv) {
 		printf("Usage: ./fconc infile1 infile2 [outfile (default:fconc.out)]\n");
 	} else {
 		
-		fd1 = open(argv[1], O_RDONLY); //file descriptor 1
-		if (fd1<0) {
-			perror(argv[1]);
-			exit(1);	
-		}
-		fd2 = open(argv[2], O_RDONLY); //file descriptor 2
-		if (fd2<0) {
-			perror(argv[2]);
-			exit(1);	
-		}
 		
-		if (argc == 3) 
-			filename = "fconc.out";
-		else 
-			filename = argv[3];
-				
 		out = open(filename,O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);	
+		write_file
+				
+		
 		
 		char buf[BUF_SIZE];
 
