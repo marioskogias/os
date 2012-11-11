4b288a10817578a7d2f2aba4f91f9e47f432c118
marioskogias <marioskogias@gmail.com>
2012-11-11 19:54:39 +0200

    First version with functions

--- 1.2/fconc.c
+++ 1.2/fconc.c
@@ -3,22 +3,33 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include<string.h>
+#include <sys/stat.h>
+
 
 #define BUF_SIZE 256
 
 void doWrite(int fd, const char *buff, int len) {
-	write(fd, buff, len);
+	
+	if (write(fd, buff, len) != len) {
+		perror("couldn't write whole buffer");
+		exit(1);
+	}
+
 }
 
 void write_file(int fd, const char *infile) {
 	int inf = open(infile, O_RDONLY); //file descriptor 1
+	int r;	
 	char buf[BUF_SIZE];
 	if (inf<0) {
 		perror(infile);
 		exit(1);	
 	}
-	while ((r1 = read(fd1,buf,BUF_SIZE))>0)
-		doWrite(fd,buff,r1)
+	while ((r = read(fd,buf,BUF_SIZE))>0)
+		doWrite(fd,buf,r);
+
+	close(inf);
+		
 
 }
 int main(int argc ,char **argv) {
@@ -33,21 +44,14 @@ int main(int argc ,char **argv) {
 		
 		
 		out = open(filename,O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);	
-		write_file
-				
-		
-		
-		char buf[BUF_SIZE];
+		if (out<0) {
+		perror("Can't create output file");
+		exit(1);	
+		}		
 
-		while ((r1 = read(fd1,buf,BUF_SIZE))>0)
-			write(out, buf, r1);
-
-		while ((r1 = read(fd2,buf,BUF_SIZE))>0)
-			write(out, buf, r1);
+		write_file(out,argv[1]);
+		write_file(out,argv[2]);
 		
-	
-		close(fd1);
-		close(fd2);
 		close(out);
 	
 	}
