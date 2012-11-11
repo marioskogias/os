c2d9a9122f74df8f206f7c3c380d440709d7c004
marioskogias <marioskogias@gmail.com>
2012-11-11 12:20:20 +0200

    Open, close, create

--- 1.2/fconc.c
+++ 1.2/fconc.c
@@ -2,21 +2,40 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include<string.h>
 
 int main(int argc ,char **argv) {
 	
-	int fd;
+	int fd1,fd2,out;
+	char * filename;
 	if (argc <3 )	{
 		printf("Usage: ./fconc infile1 infile2 [outfile (default:fconc.out)]\n");
 	} else {
 		
-		fd = open(argv[1], O_RDONLY);
-		if (fd<0) {
-			perror("open");
+		fd1 = open(argv[1], O_RDONLY); //file descriptor 1
+		if (fd1<0) {
+			perror(argv[1]);
+			exit(1);	
+		}
+		fd2 = open(argv[2], O_RDONLY); //file descriptor 2
+		if (fd2<0) {
+			perror(argv[2]);
 			exit(1);	
 		}
 		
+		if (argc == 3) 
+			filename = "fconc.out";
+		else 
+			filename = argv[3];
+			
+		out = open(filename,O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+		char buf[] = "kalhmera";
+		write(out, buf, strlen(buf));
+	
+		close(fd1);
+		close(fd2);
+		close(out);
+	
 	}
-
 	return 0;
 }
