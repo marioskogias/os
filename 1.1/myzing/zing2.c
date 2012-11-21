e9c6921f064231e4001d67336f9a1d91e0e5a1a6
marioskogias <marioskogias@gmail.com>
2012-11-21 18:23:28 +0200

    Now done my myzing

--- 1.1/myzing/zing2.c
+++ 1.1/myzing/zing2.c
@@ -1,6 +1,13 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 void zing(void) {
-	printf("Hello from my new zing %s\n",getlogin());
+	//printf("Hello from my new zing %s\n",getlogin());
+	char buf1[] = "Hello from my new zing ";
+	write(1, buf1, strlen(buf1));
+	char * buf2 = getlogin();
+	write(1,buf2,strlen(buf2));
+	write(1, "\n", 1);
+	
 }
