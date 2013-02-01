070dd9e9bab2f86f063dd16c84b0f6f2e9e6cce1
marioskogias <marioskogias@gmail.com>
2013-02-01 21:00:06 +0200

    greek comments

--- exe4/queue.h
+++ exe4/queue.h
@@ -1,18 +1,25 @@
 #ifndef QUEUE_H_
 #define QUEUE_H_
 
+/*η βασική δομή της ουράς*/
 typedef struct queue queue;
 
+/*ο "constructor"*/
 queue * init_queue() ;
 
+/*αφαίρεσε την πρώτη διεργασία*/
 struct process * dequeue(queue * q);
 
+/*επέστρεψε την πρώτη διεργασία*/
 struct process * get_top(queue *q);
 
-void enqueue(struct process *,queue * q);
+/*τοποθέτησε την δοσμένη διεργασία στο τέλος της ουράς */
+void enqueue(struct process * p,queue * q);
 
+/*διέγραψε τη διεργασία με το pid p*/
 void delete(pid_t p, queue * q);
 
+/*επέστρεψε το όνομα της διεργασίας με το pid p*/
 char * name_by_pid(pid_t p,queue * q);
 
 #endif /* REQUEST_H_ */
